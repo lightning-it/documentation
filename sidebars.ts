c54f19cabff8581ca4e20482f68bd87787f107c3
Rene Osorio <ro@l-it.io>
2026-07-25 21:57:05 -0800

    docs: define localization and search strategy

--- sidebars.ts
+++ sidebars.ts
@@ -166,6 +166,7 @@ const sidebars: SidebarsConfig = {
         "architecture/target-documentation-platform",
         "architecture/information-architecture",
         "architecture/metadata-lifecycle-model",
+        "architecture/localization-search-strategy",
       ],
     },
     {
